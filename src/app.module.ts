c4cc72afbbe522bdfb1eaf1dc7f0504b346345cd
Franz Flores <andresfloresgallardo@gmail.com>
2020-04-25 15:54:34 -0500

    Creacion del Modelo Persona

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//Conexion para la base de datos 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonModule } from './person/person.module';
+import { Person } from './person/person.entity';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'entorno',
+      entities: [Person],
+      synchronize: true
+    }),
+    PersonModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
