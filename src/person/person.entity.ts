2365b281c50028e41fa0509c869de3ae9121b5c5
Franz Flores <andresfloresgallardo@gmail.com>
2020-04-25 20:46:00 -0500

    Modelo Persona

--- src/person/person.entity.ts
+++ src/person/person.entity.ts
@@ -6,5 +6,38 @@ export class Person{
     id: number;
 
     @Column()
-    firstName: string;
+    firstName1: string;
+
+    @Column()
+    firstName2: string;
+
+    @Column()
+    lastName1: string;
+
+    @Column()
+    lastName2: string;
+
+    @Column()
+    dni: string;
+
+    @Column()
+    birthdate: Date;
+
+    @Column()
+    personalEmail: string;
+
+    @Column()
+    institutionalEmail: string;
+    
+    @Column()
+    address: string;
+
+    @Column()
+    cellphone: string;
+
+    @Column()
+    maritalStatus: string;
+
+    @Column()
+    status: boolean;
 }
